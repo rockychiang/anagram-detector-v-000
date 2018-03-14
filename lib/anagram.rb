8d7f80f98e9e2105763e79284fdd5d95bce7939d
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-14 00:00:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    words.select!{|word| word.split("").sort == @word.split("").sort}
+  end
+  
+end
